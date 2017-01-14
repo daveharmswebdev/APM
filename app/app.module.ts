81af67d56e3d7abaf80b0134011facb583e89a22
Dave Harms <daveharmswebdev@gmail.com>
2017-01-14 13:09:14 -0600

    provides product detail

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,6 +9,7 @@ import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
+import { ProductDetailGuard } from './products/product-guard.service';
 import { StarComponent } from './shared/star.component';
 
 @NgModule({
@@ -18,7 +19,11 @@ import { StarComponent } from './shared/star.component';
     HttpModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
+      { 
+        path: 'products/:id', 
+        canActivate: [ ProductDetailGuard ],
+        component: ProductDetailComponent
+      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
@@ -32,6 +37,7 @@ import { StarComponent } from './shared/star.component';
     ProductFilterPipe,
     StarComponent
   ],
+  providers: [ ProductDetailGuard ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { IProduct } from './product';
 
@@ -18,6 +20,11 @@ export class ProductService {
             .map((response: Response) => <IProduct> response.json())
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
+    }
+
+    getProduct(id: number): Observable<IProduct> {
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
     }
 
     private handleError(error: Response) {
