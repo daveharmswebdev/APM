b1c1550396875ca5f4fa4c25eea8105a0a4e4713
Dave Harms <daveharmswebdev@gmail.com>
2017-01-14 10:09:39 -0600

    created navbar with links to product list and welcome page

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -4,10 +4,8 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
-    moduleId: module.id,
-    templateUrl: 'product-list.component.html',
-    styleUrls: ['product-list.component.css']
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
